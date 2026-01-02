9655b359e8e73f2370f5d4a43027f3f2d472f121
tuskwt <smksukawatis@gmail.com>
2026-01-02 18:41:03 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,677 @@
+const appData = {
+    "bellSchedule": {
+        "Senin": [
+            {
+                "period": 1,
+                "time": "07.00 - 07.39",
+                "type": "Upacara"
+            },
+            {
+                "period": 2,
+                "time": "07.40 - 08.20"
+            },
+            {
+                "period": 3,
+                "time": "08.20 - 09.00"
+            },
+            {
+                "period": 4,
+                "time": "09.00 - 09.40"
+            },
+            {
+                "period": "Rest",
+                "time": "09.40 - 09.55",
+                "label": "Istirahat"
+            },
+            {
+                "period": 5,
+                "time": "09.55 - 10.35"
+            },
+            {
+                "period": 6,
+                "time": "10.35 - 11.15"
+            },
+            {
+                "period": 7,
+                "time": "11.15 - 11.55"
+            },
+            {
+                "period": "Rest",
+                "time": "11.55 - 12.20",
+                "label": "Istirahat"
+            },
+            {
+                "period": 8,
+                "time": "12.20 - 13.00"
+            },
+            {
+                "period": 9,
+                "time": "13.00 - 13.40"
+            },
+            {
+                "period": 10,
+                "time": "13.40 - 14.20"
+            },
+            {
+                "period": 11,
+                "time": "14.20 - 15.00"
+            }
+        ],
+        "Selasa": [
+            {
+                "period": 1,
+                "time": "07.00 - 07.45"
+            },
+            {
+                "period": 2,
+                "time": "07.45 - 08.30"
+            },
+            {
+                "period": 3,
+                "time": "08.30 - 09.15"
+            },
+            {
+                "period": "Rest",
+                "time": "09.15 - 09.30",
+                "label": "Istirahat"
+            },
+            {
+                "period": 4,
+                "time": "09.30 - 10.15"
+            },
+            {
+                "period": 5,
+                "time": "10.15 - 11.00"
+            },
+            {
+                "period": 6,
+                "time": "11.00 - 11.45"
+            },
+            {
+                "period": "Rest",
+                "time": "11.45 - 12.20",
+                "label": "Istirahat Sholat"
+            },
+            {
+                "period": 7,
+                "time": "12.20 - 13.00"
+            },
+            {
+                "period": 8,
+                "time": "13.00 - 13.40"
+            },
+            {
+                "period": 9,
+                "time": "13.40 - 14.20"
+            },
+            {
+                "period": 10,
+                "time": "14.20 - 15.00"
+            }
+        ],
+        "Rabu": [
+            {
+                "period": 1,
+                "time": "07.00 - 07.45"
+            },
+            {
+                "period": 2,
+                "time": "07.45 - 08.30"
+            },
+            {
+                "period": 3,
+                "time": "08.30 - 09.15"
+            },
+            {
+                "period": "Rest",
+                "time": "09.15 - 09.30",
+                "label": "Istirahat"
+            },
+            {
+                "period": 4,
+                "time": "09.30 - 10.15"
+            },
+            {
+                "period": 5,
+                "time": "10.15 - 11.00"
+            },
+            {
+                "period": 6,
+                "time": "11.00 - 11.45"
+            },
+            {
+                "period": "Rest",
+                "time": "11.45 - 12.20",
+                "label": "Istirahat Sholat"
+            },
+            {
+                "period": 7,
+                "time": "12.20 - 13.00"
+            },
+            {
+                "period": 8,
+                "time": "13.00 - 13.40"
+            },
+            {
+                "period": 9,
+                "time": "13.40 - 14.20"
+            },
+            {
+                "period": 10,
+                "time": "14.20 - 15.00"
+            }
+        ],
+        "Kamis": [
+            {
+                "period": 1,
+                "time": "07.00 - 07.45"
+            },
+            {
+                "period": 2,
+                "time": "07.45 - 08.30"
+            },
+            {
+                "period": 3,
+                "time": "08.30 - 09.15"
+            },
+            {
+                "period": "Rest",
+                "time": "09.15 - 09.30",
+                "label": "Istirahat"
+            },
+            {
+                "period": 4,
+                "time": "09.30 - 10.15"
+            },
+            {
+                "period": 5,
+                "time": "10.15 - 11.00"
+            },
+            {
+                "period": 6,
+                "time": "11.00 - 11.45"
+            },
+            {
+                "period": "Rest",
+                "time": "11.45 - 12.20",
+                "label": "Istirahat Sholat"
+            },
+            {
+                "period": 7,
+                "time": "12.20 - 13.00"
+            },
+            {
+                "period": 8,
+                "time": "13.00 - 13.40"
+            },
+            {
+                "period": 9,
+                "time": "13.40 - 14.20"
+            },
+            {
+                "period": 10,
+                "time": "14.20 - 15.00"
+            }
+        ],
+        "Jumat": [
+            {
+                "period": 1,
+                "time": "07.00 - 07.40",
+                "type": "P5"
+            },
+            {
+                "period": 2,
+                "time": "07.40 - 08.20"
+            },
+            {
+                "period": 3,
+                "time": "08.20 - 09.00"
+            },
+            {
+                "period": "Rest",
+                "time": "09.00 - 09.15",
+                "label": "Istirahat"
+            },
+            {
+                "period": 4,
+                "time": "09.15 - 09.55"
+            },
+            {
+                "period": 5,
+                "time": "09.55 - 10.35"
+            },
+            {
+                "period": 6,
+                "time": "10.35 - 11.15"
+            },
+            {
+                "period": 7,
+                "time": "11.15 - 11.55"
+            },
+            {
+                "period": "Rest",
+                "time": "11.55 - 12.40",
+                "label": "Istirahat 2 / Jumatan"
+            },
+            {
+                "period": 8,
+                "time": "12.40 - 13.20"
+            },
+            {
+                "period": 9,
+                "time": "13.20 - 14.00"
+            }
+        ]
+    },
+    "days": [
+        "Senin",
+        "Selasa",
+        "Rabu",
+        "Kamis",
+        "Jumat"
+    ],
+    "teachers": [
+        {
+            "id": "AK",
+            "name": "Hanshel",
+            "code": "AK",
+            "schedule": {
+                "Senin": [
+                    {
+                        "periods": [
+                            2,
+                            3
+                        ],
+                        "class": "X TKR"
+                    },
+                    {
+                        "periods": [
+                            5,
+                            6
+                        ],
+                        "class": "XI TSM.1"
+                    },
+                    {
+                        "periods": [
+                            7,
+                            8
+                        ],
+                        "class": "XI TKR"
+                    },
+                    {
+                        "periods": [
+                            10,
+                            11
+                        ],
+                        "class": "XI TSM.2"
+                    }
+                ],
+                "Selasa": [
+                    {
+                        "periods": [
+                            1,
+                            2
+                        ],
+                        "class": "X DKV & TJKT"
+                    },
+                    {
+                        "periods": [
+                            4
+                        ],
+                        "class": "XI TP"
+                    },
+                    {
+                        "periods": [
+                            5,
+                            6
+                        ],
+                        "class": "XI TSM.2"
+                    },
+                    {
+                        "periods": [
+                            7,
+                            8
+                        ],
+                        "class": "X TP"
+                    },
+                    {
+                        "periods": [
+                            9,
+                            10
+                        ],
+                        "class": "X TSM.3"
+                    }
+                ],
+                "Rabu": [
+                    {
+                        "periods": [
+                            1,
+                            2
+                        ],
+                        "class": "XI TSM.1"
+                    },
+                    {
+                        "periods": [
+                            3,
+                            4
+                        ],
+                        "class": "XI TKR"
+                    },
+                    {
+                        "periods": [
+                            5,
+                            6
+                        ],
+                        "class": "XI TP"
+                    },
+                    {
+                        "periods": [
+                            9,
+                            10
+                        ],
+                        "class": "X TSM.3"
+                    }
+                ],
+                "Kamis": [
+                    {
+                        "periods": [
+                            4,
+                            5
+                        ],
+                        "class": "X TSM.2"
+                    },
+                    {
+                        "periods": [
+                            6,
+                            7
+                        ],
+                        "class": "X TSM.1"
+                    },
+                    {
+                        "periods": [
+                            9,
+                            10
+                        ],
+                        "class": "X DKV & TJKT"
+                    }
+                ],
+                "Jumat": [
+                    {
+                        "periods": [
+                            2,
+                            3
+                        ],
+                        "class": "X TP"
+                    },
+                    {
+                        "periods": [
+                            4,
+                            5
+                        ],
+                        "class": "X TSM.2"
+                    },
+                    {
+                        "periods": [
+                            6,
+                            7
+                        ],
+                        "class": "X TKR"
+                    },
+                    {
+                        "periods": [
+                            8,
+                            9
+                        ],
+                        "class": "X TSM.1"
+                    }
+                ]
+            }
+        },
+        {
+            "id": "1767345322604",
+            "name": "Adri Caka",
+            "code": "AF",
+            "schedule": {
+                "Senin": [
+                    {
+                        "periods": [
+                            4,
+                            5
+                        ],
+                        "class": "X TP"
+                    },
+                    {
+                        "periods": [
+                            6,
+                            7
+                        ],
+                        "class": "X TKR"
+                    },
+                    {
+                        "periods": [
+                            2,
+                            3
+                        ],
+                        "class": "XI TSM.1 (B. Jawa)"
+                    }
+                ],
+                "Selasa": [
+                    {
+                        "periods": [
+                            1,
+                            2
+                        ],
+                        "class": "X TSM.3"
+                    },
+                    {
+                        "periods": [
+                            9,
+                            10
+                        ],
+                        "class": "X TJKT & DKV"
+                    },
+                    {
+                        "periods": [
+                            3,
+                            4
+                        ],
+                        "class": "XI TSM.2 (B. Jawa)"
+                    },
+                    {
+                        "periods": [
+                            5,
+                            6
+                        ],
+                        "class": "XI TKR.1 (B. Jawa)"
+                    }
+                ],
+                "Rabu": [
+                    {
+                        "periods": [
+                            4,
+                            5
+                        ],
+                        "class": "X TKR"
+                    },
+                    {
+                        "periods": [
+                            1,
+                            2
+                        ],
+                        "class": "X TSM.2"
+                    },
+                    {
+                        "periods": [
+                            9,
+                            10
+                        ],
+                        "class": "X TSM.1"
+                    },
+                    {
+                        "periods": [
+                            3
+                        ],
+                        "class": "XI TP"
+                    }
+                ],
+                "Kamis": [
+                    {
+                        "periods": [
+                            7,
+                            8
+                        ],
+                        "class": "X TSM.2"
+                    },
+                    {
+                        "periods": [
+                            5,
+                            6
+                        ],
+                        "class": "X TSM.3"
+                    },
+                    {
+                        "periods": [
+                            3,
+                            4
+                        ],
+                        "class": "XII TKR.1"
+                    },
+                    {
+                        "periods": [
+                            1,
+                            2
+                        ],
+                        "class": "X DKV & X TJKT"
+                    }
+                ],
+                "Jumat": [
+                    {
+                        "periods": [
+                            4,
+                            5
+                        ],
+                        "class": "X TP"
+                    },
+                    {
+                        "periods": [
+                            6,
+                            7
+                        ],
+                        "class": "X TSM.1"
+                    },
+                    {
+                        "periods": [
+                            8,
+                            9
+                        ],
+                        "class": "XI TP"
+                    },
+                    {
+                        "periods": [
+                            2
+                        ],
+                        "class": "XII TKR.1"
+                    }
+                ]
+            }
+        },
+        {
+            "id": "1767350803311",
+            "name": "Bang Tyox",
+            "code": "U",
+            "schedule": {
+                "Senin": [
+                    {
+                        "periods": [
+                            2,
+                            3,
+                            4,
+                            5,
+                            6,
+                            7,
+                            8,
+                            9
+                        ],
+                        "class": "XI TJKT"
+                    },
+                    {
+                        "periods": [
+                            10,
+                            11
+                        ],
+                        "class": "XII TJKT"
+                    }
+                ],
+                "Selasa": [
+                    {
+                        "periods": [
+                            1,
+                            2,
+                            3,
+                            4,
+                            5,
+                            6,
+                            7,
+                            8,
+                            9,
+                            10
+                        ],
+                        "class": "XII TJKT"
+                    }
+                ],
+                "Rabu": [
+                    {
+                        "periods": [
+                            4,
+                            5
+                        ],
+                        "class": "XI TJKT"
+                    },
+                    {
+                        "periods": [
+                            6,
+                            7,
+                            8,
+                            9,
+                            10
+                        ],
+                        "class": "XII TJKT"
+                    }
+                ],
+                "Kamis": [
+                    {
+                        "periods": [
+                            1,
+                            2,
+                            3
+                        ],
+                        "class": "XII TJKT"
+                    }
+                ],
+                "Jumat": [
+                    {
+                        "periods": [
+                            6,
+                            7,
+                            8,
+                            9
+                        ],
+                        "class": "X TJKT & X DKV"
+                    },
+                    {
+                        "periods": [
+                            2,
+                            3
+                        ],
+                        "class": "XI TJKT"
+                    }
+                ]
+            }
+        }
+    ]
+};
+
+// Validasi for CommonJS (Node) vs Browser
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = appData;
+}
